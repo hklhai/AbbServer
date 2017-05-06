2d4ab3b93109b80f22af298aa7f3f9eff5484875
hk <hklhai@163.com>
2017-05-06 09:17:19 +0800

    basic function arch

--- src/main/java/com/hxqh/abb/service/SystemService.java
+++ src/main/java/com/hxqh/abb/service/SystemService.java
@@ -6,14 +6,33 @@ import com.hxqh.abb.service.base.BaseService;
 import java.util.List;
 
 /**
- * Created by lenovo on 2017/4/14.
+ * Created by lh on 2017/4/14.
  */
 public interface SystemService extends BaseService {
 
-
+    /*============================测试开始===========================*/
     public abstract List<Location> getLocationList();
 
     public abstract long getAssetCount();
+
+    /*============================测试结束==========================*/
+
+
+
+    /*============================首页管理开始========================*/
+
+
+
+    /*============================首页管理结束 =======================*/
+
+
+    /*============================现场及设备地图开始===================*/
+
+
+
+    /*============================现场及设备地图结束 ==================*/
+
+
 
 
 }
