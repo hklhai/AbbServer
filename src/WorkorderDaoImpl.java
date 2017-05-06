3a3aed488b6a1c3f066614fbf8035ffb5d37e141
hk <hklhai@163.com>
2017-05-06 14:33:46 +0800

    index message

---
+++ src/WorkorderDaoImpl.java
@@ -0,0 +1,17 @@
+
+package hxqh.eam.abb.dao;
+
+import com.hxqh.abb.common.basedao.DaoSupport;
+import com.hxqh.abb.model.Workorder;
+import org.springframework.stereotype.Repository;
+
+
+/**
+ *
+ * @author lh
+ *
+ */
+@Repository("workorderDao")
+public class WorkorderDaoImpl extends DaoSupport<Workorder> implements WorkorderDao {
+
+}
---
+++ src/main/java/com/hxqh/abb/dao/WorkorderDao.java
@@ -0,0 +1,15 @@
+
+package com.hxqh.abb.dao;
+
+import com.hxqh.abb.common.basedao.Dao;
+import com.hxqh.abb.model.Workorder;
+
+
+/**
+ *
+ * @author lh
+ *
+ */
+public interface WorkorderDao extends Dao<Workorder> {
+}
+
