9119093401b25abc9b3e632f32aafc8345310fc2
hk <hklhai@163.com>
2017-05-08 18:28:55 +0800

    index show list

--- src/main/java/com/hxqh/abb/controller/IndexController.java
+++ src/main/java/com/hxqh/abb/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.hxqh.abb.controller;
 
 import com.hxqh.abb.common.util.FastJsonTools;
+import com.hxqh.abb.common.util.MXCipherXUtils;
 import com.hxqh.abb.model.Location;
 import com.hxqh.abb.model.Maxuser;
 import com.hxqh.abb.model.Wfassignment;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import psdi.util.MXException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -61,24 +63,35 @@ public class IndexController {
     public ModelAndView login(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
         Map<String, Object> result = new HashMap<String, Object>();
         String loginid = httpServletRequest.getParameter("loginid");
-        String userid = httpServletRequest.getParameter("userid");
-        LoginDto loginDto = new LoginDto(loginid,userid);
-        List<Maxuser> loginUserList = systemService.getLoginUserList(loginDto);
-        Message message=doLogin(loginUserList);
+        String password = httpServletRequest.getParameter("password");
+        LoginDto loginDto = new LoginDto(loginid,password);
+        List<Maxuser> loginUserList =  systemService.getLoginUserList(loginDto);
+
+        Message message=doLogin(loginUserList,loginDto);
         FastJsonTools.writeJson(message,httpServletResponse);
         return new ModelAndView("/success",result);
-
     }
 
-    private Message doLogin(List<Maxuser> loginUserList) {
+    private Message doLogin(List<Maxuser> loginUserList,LoginDto loginDto) {
         Message message = new Message(0,"",false);
         Message success = new Message(1,"LoginSuccess",true);
 
         if(loginUserList.size()>0)
         {
-            return success;
+            String password = null;
+            try {
+                password = MXCipherXUtils.encodePwd(loginDto.getPassword());
+                if(loginUserList.get(0).getPassword().toUpperCase().equals(password))
+                {
+                    return success;
+                }
+            } catch (MXException e) {
+                e.printStackTrace();
+                Message messageException = new Message(1,"异常",false);
+                return messageException;
+            }
         }else {
-            message.setMessage("用户名或密码错误");
+            message.setMessage("用户名不存在");
             message.setSuccess(true);
         }
         return  message;
--- src/test/java/com/hxqh/abb/test/SystemTest.java
+++ src/test/java/com/hxqh/abb/test/SystemTest.java
@@ -1,5 +1,10 @@
 package com.hxqh.abb.test;
 
+import com.hxqh.abb.common.util.MXCipherXUtils;
+import com.hxqh.abb.dao.AssetDao;
+import com.hxqh.abb.dao.MaxuserDao;
+import com.hxqh.abb.dao.WfassignmentDao;
+import com.hxqh.abb.dao.WorkorderDao;
 import com.hxqh.abb.model.Location;
 import com.hxqh.abb.model.Maxuser;
 import com.hxqh.abb.model.dto.LoginDto;
@@ -9,6 +14,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import psdi.util.MXException;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
@@ -23,6 +29,17 @@ import java.util.List;
 public class SystemTest {
     @Resource
     private SystemService systemService;
+
+    @Resource
+    private AssetDao assetDao;
+    @Resource
+    private WorkorderDao workorderDao;
+
+    @Resource
+    private WfassignmentDao wfassignmentDao;
+
+    @Resource
+    private MaxuserDao maxuserDao;
 
     @Test
     public void testGetLocation() {
@@ -39,22 +56,66 @@ public class SystemTest {
     //@Test
     public void testLocationName() {
         List<Location> locationList = systemService.getLocationList();
-        for (Location l:locationList) {
+        for (Location l : locationList) {
             System.out.println(l);
             System.out.println("=======================");
         }
     }
 
+    /**
+     * login Test
+     */
     @Test
-    public void testGetLoginUserList() {
-        LoginDto loginDtoSu = new LoginDto("maxadmin","MAXADMIN");
-        LoginDto loginDtoFail = new LoginDto("maxadmin","xxx");
+    public void testGetLoginUserList() throws Exception {
+        LoginDto loginDtoSu = new LoginDto("maxadmin", "maxadmin");
+        LoginDto loginDtoFail = new LoginDto("maxadmin", "xxx");
 
         List<Maxuser> loginUserList = systemService.getLoginUserList(loginDtoSu);
         Assert.assertEquals(1, loginUserList.size());
-
         List<Maxuser> loginUserList2 = systemService.getLoginUserList(loginDtoFail);
         Assert.assertEquals(0, loginUserList2.size());
+
+
     }
+
+    public static String toHexString(String s) {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            int ch = (int) s.charAt(i);
+            String s4 = Integer.toHexString(ch);
+            str = str + s4;
+        }
+        return str;
+    }
+
+    /**
+     * Index page data access Test
+     */
+    @Test
+    public void testIndexAccess() {
+        Assert.assertEquals(257, assetDao.findAll().size());
+        Assert.assertEquals(8, workorderDao.findAll().size());
+        Assert.assertEquals(1438, wfassignmentDao.findAll().size());
+        //System.out.println(workorderDao.findAll().size());
+        //System.out.println(wfassignmentDao.findAll().size());
+    }
+
+    /**
+     * 密码测试 通过 2017-5-8 16:51:37
+     */
+    @Test
+    public void testLoginByMaximo() {
+        String password = "maxadmin";
+        String pwd = null;
+        try {
+            pwd = MXCipherXUtils.encodePwd(password);
+        } catch (MXException e) {
+            e.printStackTrace();
+        }
+        System.out.println(pwd);
+        Assert.assertEquals("10FE6F4650B2ACB49A2121D7E6133E64", pwd);
+    }
+
+
 
 }
