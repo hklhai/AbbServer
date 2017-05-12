364dfd531f84b752c10515ac034c96650f918988
hkhai <hklhai@outlook.com>
2017-05-12 15:52:24 +0800

    interceptors add to prompt security，but not used for development and debug convenient

--- src/main/java/com/hxqh/abb/controller/IndexController.java
+++ src/main/java/com/hxqh/abb/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.hxqh.abb.controller;
 import com.hxqh.abb.common.util.MXCipherXUtils;
 import com.hxqh.abb.model.Location;
 import com.hxqh.abb.model.Maxuser;
+import com.hxqh.abb.model.base.SessionInfo;
 import com.hxqh.abb.model.dto.IndexDto;
 import com.hxqh.abb.model.dto.LoginDto;
 import com.hxqh.abb.model.dto.Message;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 import psdi.util.MXException;
 
@@ -25,6 +27,7 @@ import java.util.Map;
 
 @Controller
 @RequestMapping("/index")
+@SessionAttributes(value = "sessionInfo")
 public class IndexController {
     @Autowired
     private SystemService systemService;
@@ -34,14 +37,15 @@ public class IndexController {
         Map<String, Object> result = new HashMap<String, Object>();
         List<Location> locationList = systemService.getLocationList();
         result.put("locationList", locationList);
-        result.put("size",locationList.size());
-        return new ModelAndView("/success",result);
+        result.put("size", locationList.size());
+        return new ModelAndView("/success", result);
     }
 
     /**
      * 返回页面4部分显示List
-     * @Author lh
+     *
      * @return
+     * @Author lh
      */
     @RequestMapping(value = "/message", method = RequestMethod.GET)
     @ResponseBody
@@ -63,43 +67,45 @@ public class IndexController {
      */
     @ResponseBody
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Message login(LoginDto loginDto) {
-        List<Maxuser> loginUserList =  systemService.getLoginUserList(loginDto);
-        return doLogin(loginUserList,loginDto);
+    public Message login(LoginDto loginDto, Map<String, Object> map) {
+        List<Maxuser> loginUserList = systemService.getLoginUserList(loginDto);
+        return doLogin(loginUserList, loginDto, map);
     }
 
-    private Message doLogin(List<Maxuser> loginUserList,LoginDto loginDto) {
-        Message message = new Message(0,"",false);
-        Message success = new Message(1,"LoginSuccess",true);
+    private Message doLogin(List<Maxuser> loginUserList, LoginDto loginDto, Map<String, Object> map) {
+        Message message = new Message(0, "", false);
+        Message success = new Message(1, "LoginSuccess", true);
 
-        if(loginUserList.size()>0)
-        {
+        if (loginUserList.size() > 0) {
             String password = null;
             try {
                 password = MXCipherXUtils.encodePwd(loginDto.getPassword());
-                if(loginUserList.get(0).getPassword().toUpperCase().equals(password))
-                {
+                if (loginUserList.get(0).getPassword().toUpperCase().equals(password)) {
+                    //加入Session中
+                    Maxuser login = loginUserList.get(0);
+                    SessionInfo sessionInfo = new SessionInfo(login.getLoginid(), login.getDefsite(), " ");
+                    map.put("sessionInfo", sessionInfo);
                     return success;
-                }else
-                {
+                } else {
                     message.setMessage("密码不正确");
                     message.setSuccess(true);
                 }
             } catch (MXException e) {
                 e.printStackTrace();
-                Message messageException = new Message(1,"异常",false);
+                Message messageException = new Message(1, "异常", false);
                 return messageException;
             }
-        }else {
+        } else {
             message.setMessage("用户名不存在");
             message.setSuccess(true);
         }
-        return  message;
+        return message;
     }
 
 
     /**
      * 登录成功后跳转至index
+     *
      * @return
      */
     @RequestMapping(value = "/toIndex", method = RequestMethod.GET)
---
+++ src/main/java/com/hxqh/abb/model/base/SessionInfo.java
@@ -0,0 +1,46 @@
+package com.hxqh.abb.model.base;
+
+/**
+ * Created by lh on 2017-05-12.
+ */
+public class SessionInfo {
+
+    private String loginId;
+
+    private String siteid;
+
+    private String orgid;
+
+    public SessionInfo() {
+    }
+
+    public SessionInfo(String loginId, String siteid, String orgid) {
+        this.loginId = loginId;
+        this.siteid = siteid;
+        this.orgid = orgid;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getSiteid() {
+        return siteid;
+    }
+
+    public void setSiteid(String siteid) {
+        this.siteid = siteid;
+    }
+
+    public String getOrgid() {
+        return orgid;
+    }
+
+    public void setOrgid(String orgid) {
+        this.orgid = orgid;
+    }
+}
