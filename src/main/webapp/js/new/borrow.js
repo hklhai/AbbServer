03d6f4a73298ea3e7b5cd5ec5eaca5be46a8a664
aym19900825 <825185228@qq.com>
2017-08-24 17:40:49 +0800

    list 调试

--- src/main/webapp/js/new/borrow.js
+++ src/main/webapp/js/new/borrow.js
@@ -1,24 +1,54 @@
 $(function() {
-    $.ajax({
-        url: _ctx + "/common/listData",
-        method: "get",
-        data: {
-            apptname: apptname,
-            apptable: 'UDVEHICLE',
-            pkid: 'UDVEHICLEID',
-            fields: 'LICENSE,DESCRIPTION,MODEL,FACTORY,SITEID,UDVEHICLEID' ,
-            searchs: ''
+    function commonList(){
+        this.names = [];
+    }
+    commonList.prototype = {
+        constructor: commonList,
+        initData: function(){
+            var self = this;
+            $.ajax({
+                url: _ctx + "/common/listData",
+                method: "get",
+                data: {
+                    apptname: apptname,
+                    apptable: 'UDVEHICLE',
+                    pkid: 'UDVEHICLEID',
+                    fields: 'LICENSE,DESCRIPTION,MODEL,FACTORY,SITEID,UDVEHICLEID' ,
+                    searchs: ''
+                },
+                dataType: "json",
+                success: function (data) {
+                    self.initNames();
+                    self.initDom(data.list);
+                },
+                error: function () {
+
+                }
+            });
         },
-        dataType: "json",
-        success: function (data) {
-            var listData =  data.list;
-            var tmpHtml = "";
-            for(var i = 0;i<listData.length;i++){
-                
+        initNames:function(){
+            var self = this;
+            var namesDom = $(".mytable tbody tr:nth-child(1)").find("td");
+            for(var i = 0;i<namesDom.length-1;i++){
+                var val = namesDom.eq(i).find('input').first().attr("name").toLowerCase();
+                self.names.push(val);
             }
         },
-        error: function () {
-
+        initDom:function(listData){
+            var self = this;
+            var tmpHtml = "";
+            for(var i = 0;i<listData.length;i++){
+                var thisName;
+                tmpHtml +="<tr>";
+                for(var j=0;j<self.names.length;j++){
+                    thisName = self.names[j];
+                    tmpHtml+="<td>"+listData[i][thisName]+"</td>";
+                };
+                tmpHtml+="</tr>";
+            }
+            $(".mytable tbody tr").remove(":not(:first)");
+            $(".mytable tbody").append(tmpHtml);
         }
-    });
+    }
+    new commonList().initData();
 });
